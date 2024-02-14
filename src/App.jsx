48ff67b971f4ce4cadea2b21a3794069294314f3
Giiid <gideonfummey1@gmail.com>
2024-02-14 12:16:15 +0000

    new

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ function App() {
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0eBYynT_gghHTj2kbyGTpxnmNloadT7uU8w&usqp=CAU"
           />
           <div className="text">Yayyyyy!!!!</div>
+          console.log("Yes")
         </>
       ) : (
         <>
@@ -67,4 +68,4 @@ function App() {
   )
 }
 
-// export default App
+export default App
